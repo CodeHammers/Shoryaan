9693e1710327310dc5795e6087d614ea6ca0d585
Kareem <kareememad400@gmail.com>
2018-04-20 17:05:34 +0200

    add managers view linked to api --pending-new-manager

--- components/hospital/managers.js
+++ components/hospital/managers.js
@@ -1,15 +1,40 @@
 import React from 'react'
-import { Container, Header, Item, Input, Icon,Title, Button, Text, CheckBox, Body, ListItem, Picker, Content, List, Left, Right, Thumbnail } from 'native-base';
+import { Container, Header, Item, Input, Icon,Title, Button, Text, CheckBox, Form,Body, ListItem, Picker, Content, List, Left, Right, Thumbnail } from 'native-base';
 import {StyleSheet, View, ScrollView, StatusBar} from 'react-native'
+
+import {AuthService} from '../../services/auth'
+
 
 export class Managers extends React.Component
 {
 
     constructor(props){
         super(props)
+        const { params } = this.props.navigation.state;
+
         this.state ={
-            arrayholder:[1,2,3,4,5,6]
+            managers:[],
+            id: params.id,
+            auth_service: new AuthService(this) //instance from an authentication service
+
         }
+        this.getManagers()
+    }
+    getManagers(){
+        this.state.auth_service.get('/hospital/hospital_users?id='+this.state.id)
+        .then((res)=> { return res.json() } )
+        .then((res_json)=>{
+            this.setState({managers: res_json })
+        })
+    }
+
+    setManager(){
+        alert("Not implement in frontEnd yet")
+        this.state.auth_service.get('/hospital/hospital_users?id='+this.state.id+"&user_email="+this.state.email)
+        .then((res)=> { return res.json() } )
+        .then((res_json)=>{
+            this.setState({managers: res_json })
+        })
     }
 
     /** A function that renders the actual view */
@@ -34,18 +59,18 @@ export class Managers extends React.Component
                     </Right>
                 </Header>
 
-                 <Text> all people managing hospital + ability to add more</Text>
+                 <Text> Managers</Text>
 
                 <View>
 
-                    <List dataArray={this.state.arrayholder} renderRow={(arrayholder) =>
-                        <ListItem avatar button={true} >
+                    <List dataArray={this.state.managers} renderRow={(arrayholder) =>
+                        <ListItem avatar button={true}  style={{marginTop:17}}>
                             <Left>
                                 <Thumbnail source={require('../../hos.png')} />
                             </Left>
                             <Body>
-                                <Text style={styles.listitemname}>{'david'}</Text>
-                                <Text style={styles.StatePickerItem} note>{'david'}</Text>
+                                <Text style={styles.listitemname}>{arrayholder.name}</Text>
+                                <Text style={styles.StatePickerItem} note>{arrayholder.email}</Text>
                             </Body>
                             <Right>
                                 <Text style={styles.StatePickerItem} note>{'manager'}</Text>
@@ -54,6 +79,21 @@ export class Managers extends React.Component
                     }>
                     </List>
 
+             <Form style={{margin:15}}>
+
+                    <Item style={{marginTop:8,marginBottom:8}} >
+                                <Input   
+                                    placeholder='Email'
+                                    onChangeText={(text) =>{ this.setState({content: text});}}
+
+                                />
+                        
+                    </Item>
+                    <Button rounded  danger sytle={{marginTop:12}} >
+                        <Text>Add New Manager </Text>
+                    </Button>
+            </Form>
+               
 
 
                 </View>
